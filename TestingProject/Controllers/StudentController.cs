b8cdad26e1cba8f9a0afa5979edd676b4e234758
Shihabul Hasan Shihab <6shihab@gmail.com>
2022-11-18 12:57:57 +0600

    Project Commited

---
+++ TestingProject/Controllers/StudentController.cs
@@ -0,0 +1,74 @@
+﻿using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using TestingProject.Models;
+
+namespace TestingProject.Controllers
+{
+    public class StudentController : Controller
+    {
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "XrEHe8CrorAy8rHHZ8K3wuIDPRbJS2CDoadxXI7V",
+            BasePath = "https://studentcrudapp-9daa9-default-rtdb.firebaseio.com/"
+        };
+        IFirebaseClient? client;
+
+        public ActionResult Index()
+        {
+            client = new FireSharp.FirebaseClient(config);
+            FirebaseResponse response = client.Get("Students");
+            dynamic data = JsonConvert.DeserializeObject<dynamic>(response.Body);
+            var list = new List<Student>();
+            foreach (var item in data)
+            {
+                list.Add(JsonConvert.DeserializeObject<Student>(((JProperty)item).Value.ToString()));
+            }
+
+            return View(list);
+
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            try
+            {
+                addStudentToFirebase(student);
+                ModelState.AddModelError(String.Empty,"Added Successfully");
+
+            }catch (Exception ex)
+            {
+                ModelState.AddModelError(String.Empty, ex.Message);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        private void addStudentToFirebase(Student student)
+        {
+            client=new FireSharp.FirebaseClient(config);
+            var data = student;
+            PushResponse response = client.Push("Students/", data);
+            data.Id = response.Result.name;
+            SetResponse setResponse = client.Set("Students/"+data.Id,data);
+        }
+
+        [HttpGet]
+        public ActionResult Delete(string id)
+        {
+            client = new FireSharp.FirebaseClient(config);
+            FirebaseResponse response = client.Delete("Students/" + id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ TestingProject/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace TestingProject.Models
+{
+    public class Student
+    {
+        public string Id { get; set; }
+        public string student_id { get; set; }
+        public string fullname { get; set; }
+        public string degree_title { get; set; }
+        public string address { get; set; }
+        public string phone { get; set; }
+
+    }
+}
